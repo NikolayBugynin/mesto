8f0b68434ccf89a190d8da597937196da7d1a090
Nikolay Bugynin <bugynin@yandex.ru>
2022-11-17 19:28:23 +0300

    v3.hope final:)

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,15 @@ let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
 let formElement = document.querySelector('.popup__form');
+let titleProfile = document.querySelector(".profile__title");
 let nameInput = document.querySelector('.popup__item_type_name');
 let jobInput = document.querySelector('.popup__item_type_specification');
+let specificationProfile = document.querySelector('.profile__specification');
 
 function popupOpen() {
     popup.classList.add('popup_opened');
-    namePopup.value = nameProfile.textContent;
-    specificationPopup.value = specificationProfile.textContent;
+    nameInput.value = titleProfile.textContent;
+    jobInput.value = specificationProfile.textContent;
 }
 editButton.addEventListener('click', popupOpen);
 
@@ -21,7 +23,7 @@ function formSubmitHandler(evt) {
     evt.preventDefault();
     document.querySelector('.profile__title').textContent = nameInput.value;
     document.querySelector('.profile__specification').textContent = jobInput.value;
-    popupOpen.classList.remove('popup_opened');
+    popupClose();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
